fe1e53e68df07df30f88cc9c37e57fc1d3cb6110
zeta <yeomfilm@gmail.com>
2023-05-16 12:30:59 +0900

    pt to tfjs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Wildet
 
-> **_딥러닝 기반 야생동물객체탐지 어플리케이션_**
+> **딥러닝 기반 야생동물 객체 탐지 어플리케이션**
 
 <br/>
 
@@ -19,43 +19,49 @@ Wildet은 딥러닝 기반 야생동물 객체 탐지 어플리케이션 입니�
 **▶️ 목적**
 
 딥러닝 기반 야생동물 탐지 어플리케이션 개발  
-➡️ 야생동물과의 충돌사고 예방, 야생동물 생태 조사를 통한 보호 등의 목적으로 활용
+-> 야생동물과의 충돌사고 예방, 야생동물 생태 조사를 통한 보호 등의 목적으로 활용
 
 <br/>
 
-### 기술스택
+### **기술스택**
 
 ---
 
-<img src="https://img.shields.io/badge/react-61DAFB?style=for-the-badge&logo=react&logoColor=black">
-<img src="https://img.shields.io/badge/firebase-FFCA28?style=for-the-badge&logo=firebase&logoColor=white">
-<img src="https://img.shields.io/badge/python-3776AB?style=for-the-badge&logo=python&logoColor=white">
-<img src="https://img.shields.io/badge/flask-000000?style=for-the-badge&logo=flask&logoColor=white">
-<img src="https://img.shields.io/badge/yolov5-00FFFF?style=for-the-badge&logo=yolo&logoColor=white">
-<img src="https://img.shields.io/badge/pytorch-EE4C2C?style=for-the-badge&logo=pytorch&logoColor=white">
+<img src="https://img.shields.io/badge/react-61DAFB?style=for-the-badge&logo=react&logoColor=black"><img src="https://img.shields.io/badge/firebase-FFCA28?style=for-the-badge&logo=firebase&logoColor=white"><img src="https://img.shields.io/badge/python-3776AB?style=for-the-badge&logo=python&logoColor=white"><img src="https://img.shields.io/badge/flask-000000?style=for-the-badge&logo=flask&logoColor=white"><img src="https://img.shields.io/badge/yolov5-00FFFF?style=for-the-badge&logo=yolo&logoColor=white"><img src="https://img.shields.io/badge/pytorch-EE4C2C?style=for-the-badge&logo=pytorch&logoColor=white">
 
 <br/>
 
-### 시스템 구조
+### **시스템 구조**
 
 ---
 
-<img src="/Users/minjeongyeom/Projects/project-wildet/wildet/public/wildet-sys.png">
+<img width="491" alt="wildet-sys" src="https://user-images.githubusercontent.com/64299109/226230419-be68d566-c152-4f43-97dd-59b8c344baa9.png">
 
 <br/>
 
-### 팀원 소개 👩‍💻
+### **팀원 소개** 👩‍💻
 
 ---
 
 | 이름   | 역할                                                                                                                              |
 | ------ | --------------------------------------------------------------------------------------------------------------------------------- |
-| 염민정 | - 데이터 전처리 <br />- yolov5 알고리즘 모델 Cutrom Training <br />- UI 및 Flask 개발 <br />- Firebase 로그인, 회원가입 기능 개발 |
+| 염민정 | - 데이터 전처리 <br />- yolov5 알고리즘 모델 Custom Training <br />- UI 및 Flask 개발 <br />- Firebase 로그인, 회원가입 기능 개발 |
 | 고민정 | - UI 개발<br />- React Webcam 기능 개발                                                                                           |
+
+<br />
+### **기능소개**
+
+---
+
+<br />
+
+### **기대효과**
+
+---
 
 <br/>
 
-### 기간
+### **기간 📆**
 
 ---
 
@@ -63,7 +69,7 @@ Wildet은 딥러닝 기반 야생동물 객체 탐지 어플리케이션 입니�
 
 <br/>
 
-### 성과
+### **성과**
 
 ---
 
--- wildet/src/components/WebcamVideo.jsx
+++ wildet/src/components/WebcamVideo.jsx
@@ -1,117 +1,181 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Webcam from "react-webcam";
 import axios from "axios";
 import Analysising from "./Analysising";
 
+// export default function WebcamVideo() {
+//   const webcamRef = useRef(null);
+//   const mediaRecorderRef = useRef(null);
+//   const [recording, setRecording] = useState(false);
+//   const [videoUrl, setVideoUrl] = useState("");
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   const handleStartRecording = () => {
+//     setRecording(true);
+//     mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream);
+//     mediaRecorderRef.current.addEventListener(
+//       "dataavailable",
+//       handleDataAvailable
+//     );
+//     mediaRecorderRef.current.start();
+//   };
+
+//   const handleDataAvailable = (event) => {
+//     if (event.data.size > 0) {
+//       const videoBlob = new Blob([event.data], { type: "video/mp4" });
+//       const formData = new FormData();
+//       formData.append("file", videoBlob);
+//       setIsLoading(true);
+//       axios
+//         .post("http://localhost:5000/detect", formData, {
+//           headers: {
+//             "Content-Type": "multipart/form-data",
+//           },
+//           withCredentials: true,
+//           responseType: "blob", // blob 유형으로 응답 받음
+//         })
+//         .then((response) => {
+//           console.log("성공");
+
+//           const videoBlob = new Blob([response.data], {
+//             type: "video/mp4",
+//           });
+
+//           setIsLoading(false);
+
+//           const videoUrl1 = URL.createObjectURL(videoBlob); // URL 객체로 변환
+//           setVideoUrl(videoUrl1);
+
+//           // const a = document.createElement("a");
+//           // a.href = videoUrl1;
+//           // const videoName = `video_${new Date().getTime()}.mp4`;
+//           // a.download = videoName;
+//           // a.click();
+//         });
+//     }
+//   };
+
+//   console.log(videoUrl);
+
+//   const handleStopRecording = () => {
+//     mediaRecorderRef.current.stop();
+//   };
+
+//   const videoConstraints = {
+//     width: 420,
+//     height: 420,
+//     facingMode: "user",
+//   };
+
+//   // const handleVideoLoaded = (event) => {
+//   //   event.target.play();
+//   // };
+
+//   return (
+//     <div className="Container">
+//       {isLoading ? (
+//         <div
+//           style={{
+//             color: "white",
+//           }}
+//         >
+//           객체 인식 중
+//         </div>
+//       ) : (
+//         <>
+//           {videoUrl ? (
+//             <button
+//               onClick={() => {
+//                 const a = document.createElement("a");
+//                 a.href = videoUrl;
+//                 const videoName = `video_${new Date().getTime()}.mp4`;
+//                 a.download = videoName;
+//                 a.click();
+//               }}
+//             >
+//               다운로드
+//             </button>
+//           ) : (
+//             <Webcam
+//               height={400}
+//               width={400}
+//               audio={false}
+//               mirrored={true}
+//               ref={webcamRef}
+//               videoConstraints={videoConstraints}
+//             />
+//           )}
+//           {recording ? (
+//             <button onClick={handleStopRecording}>Stop Recording</button>
+//           ) : (
+//             <button onClick={handleStartRecording}>Start Recording</button>
+//           )}
+//         </>
+//       )}
+//     </div>
+//   );
+// }
+
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs";
+
 export default function WebcamVideo() {
-  const webcamRef = useRef(null);
-  const mediaRecorderRef = useRef(null);
-  const [recording, setRecording] = useState(false);
-  const [videoUrl, setVideoUrl] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const videoRef = useRef(null);
 
-  const handleStartRecording = () => {
-    setRecording(true);
-    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream);
-    mediaRecorderRef.current.addEventListener(
-      "dataavailable",
-      handleDataAvailable
-    );
-    mediaRecorderRef.current.start();
-  };
+  useEffect(() => {
+    const loadModelAndDetect = async () => {
+      // TensorFlow.js 모델을 로드합니다.
+      const model = await tf.loadLayersModel("../detect/tfjs_model/model.json");
 
-  const handleDataAvailable = (event) => {
-    if (event.data.size > 0) {
-      const videoBlob = new Blob([event.data], { type: "video/mp4" });
-      const formData = new FormData();
-      formData.append("file", videoBlob);
-      setIsLoading(true);
-      axios
-        .post("http://localhost:5000/detect", formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-          withCredentials: true,
-          responseType: "blob", // blob 유형으로 응답 받음
-        })
-        .then((response) => {
-          console.log("성공");
+      const detectObjects = () => {
+        // 비디오 프레임을 캡처합니다.
+        const video = videoRef.current;
+        const image = tf.browser.fromPixels(video);
+        const expandedImage = image.expandDims(0);
 
-          const videoBlob = new Blob([response.data], {
-            type: "video/mp4",
-          });
+        // 객체 인식을 수행합니다.
+        const predictions = model.predict(expandedImage);
 
-          setIsLoading(false);
+        // 결과를 처리하고 화면에 실시간 객체 인식 결과를 표시하는 로직을 구현합니다.
+        // predictions를 활용하여 실시간으로 객체 인식 결과를 화면에 표시하는 코드를 작성하세요.
+        // 예시:
+        // const boxes = await predictions[0].array();
+        // const classes = await predictions[1].array();
+        // const scores = await predictions[2].array();
+        // console.log(boxes, classes, scores);
 
-          const videoUrl1 = URL.createObjectURL(videoBlob); // URL 객체로 변환
-          setVideoUrl(videoUrl1);
+        // 다음 프레임을 처리하기 위해 재귀적으로 호출합니다.
+        requestAnimationFrame(detectObjects);
+      };
 
-          // const a = document.createElement("a");
-          // a.href = videoUrl1;
-          // const videoName = `video_${new Date().getTime()}.mp4`;
-          // a.download = videoName;
-          // a.click();
-        });
-    }
-  };
+      // 객체 인식을 시작합니다.
+      detectObjects();
+    };
 
-  console.log(videoUrl);
-
-  const handleStopRecording = () => {
-    mediaRecorderRef.current.stop();
-  };
-
-  const videoConstraints = {
-    width: 420,
-    height: 420,
-    facingMode: "user",
-  };
-
-  // const handleVideoLoaded = (event) => {
-  //   event.target.play();
-  // };
+    // 웹캠 스트림에 접근하고 객체 인식을 시작합니다.
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.onloadedmetadata = () => {
+          loadModelAndDetect();
+        };
+      })
+      .catch((error) => {
+        console.log("웹캠 접근 오류:", error);
+      });
+  }, []);
 
   return (
     <div className="Container">
-      {isLoading ? (
-        <div
-          style={{
-            color: "white",
-          }}
-        >
-          객체 인식 중
-        </div>
-      ) : (
-        <>
-          {videoUrl ? (
-            <button
-              onClick={() => {
-                const a = document.createElement("a");
-                a.href = videoUrl;
-                const videoName = `video_${new Date().getTime()}.mp4`;
-                a.download = videoName;
-                a.click();
-              }}
-            >
-              다운로드
-            </button>
-          ) : (
-            <Webcam
-              height={400}
-              width={400}
-              audio={false}
-              mirrored={true}
-              ref={webcamRef}
-              videoConstraints={videoConstraints}
-            />
-          )}
-          {recording ? (
-            <button onClick={handleStopRecording}>Stop Recording</button>
-          ) : (
-            <button onClick={handleStartRecording}>Start Recording</button>
-          )}
-        </>
-      )}
+      <video
+        id="video"
+        width={400}
+        height={400}
+        autoPlay
+        ref={videoRef}
+        style={{ transform: "scaleX(-1)" }}
+      />
     </div>
   );
 }
